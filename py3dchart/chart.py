9b0f4a5d4860100367b68786ab3eebbd49762887
Nick <nickyoung0017@outlook.com>
2023-04-13 22:51:31 +0800

    second

--- py3dchart/chart.py
+++ py3dchart/chart.py
@@ -2,60 +2,45 @@ from tkinter import *
 import numpy
 import math
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'WangLvyuan'
 
 class chart3d:
-    fontSize = 12
-    x1 = 20
-    y1 = 20
-    x2 = 220
-    y2 = 220
+    
+    fontSize1 = 12
+    xx1 = 20
+    yy1 = 20
+    xx2 = 220
+    yy2 = 220
 
     def fontSize(self, num):
-         chart3d.fontSize = num
+         chart3d.fontSize1 = num
          return
 
     def x1(self, num):
-         chart3d.x1 = num
+         chart3d.xx1 = num
          return
     
     def x2(self, num):
-         chart3d.x2 = num
+         chart3d.xx2 = num
          return
     
     def y1(self, num):
-         chart3d.y1 = num
+         chart3d.yy1 = num
          return
     
     def y2(self, num):
-         chart3d.y2 = num
+         chart3d.yy2 = num
          return
 
-    def pieData(data2):
-        rate = []
-        s = sum(data2)
-        for x in data2:
-            rate.append('{:.4f}'.format(x/s))
-        rate = numpy.array(rate, dtype = numpy.float64)
-        return rate
-    
-    def locationCal(degree):
-        rad = math.radians(degree)
-        xLocation1 = round(round(math.cos(-rad),2) * (chart3d.x2-chart3d.x1)/4 + (chart3d.x2+chart3d.x1)/2)
-        yLocation1 = round(round(math.sin(-rad),2) * (chart3d.x2-chart3d.x1)/4 + (chart3d.y2+chart3d.y1)/2)
-        xLocation2 = round(round(math.cos(-rad),2) * (chart3d.x2-chart3d.x1)/2-10 + (chart3d.x2+chart3d.x1)/2)
-        yLocation2 = round(round(math.sin(-rad),2) * (chart3d.x2-chart3d.x1)/2-10 + (chart3d.y2+chart3d.y1)/2)
-        return [xLocation1, yLocation1, xLocation2, yLocation2]
-    
-    # the pie apart
+# the pie apart
     def drawPie(self, pie_data_as_array, pie_lable_as_llist):
         rate = chart3d.pieData(pie_data_as_array)
         root = Tk()
-        coord = chart3d.x1, chart3d.y1, chart3d.x2, chart3d.y2
-        shadow = chart3d.x1 + 10, chart3d.y1 + 10, chart3d.x2 + 10, chart3d.y2 + 10
-        center = chart3d.x1 + 80, chart3d.y1 + 80, chart3d.x2 - 80, chart3d.y2 - 80
-        centerShadow = chart3d.x1 + 90, chart3d.y1 + 90, chart3d.x2 - 70, chart3d.y2 - 70
+        coord = chart3d.xx1, chart3d.yy1, chart3d.xx2, chart3d.yy2
+        shadow = chart3d.xx1 + 10, chart3d.yy1 + 10, chart3d.xx2 + 10, chart3d.yy2 + 10
+        center = chart3d.xx1 + 80, chart3d.yy1 + 80, chart3d.xx2 - 80, chart3d.yy2 - 80
+        centerShadow = chart3d.xx1 + 90, chart3d.yy1 + 90, chart3d.xx2 - 70, chart3d.yy2 - 70
         cv = Canvas(root, bg='white')
         cv.pack(fill = 'both',expand ='yes')
     
@@ -70,20 +55,32 @@ class chart3d:
             location = chart3d.locationCal(startPoint + degree/2)#data type: int in list
             cv.create_arc(coord, start=startPoint, extent=degree, fill="white")
             cv.create_text(location[0], location[1], text=str(round((x*100),1)) + '%')
-            cv.create_text(location[2], location[3], text=y, font=("Arial", chart3d.fontSize))
+            cv.create_text(location[2], location[3], text=y, font=("Arial", chart3d.fontSize1))
             location = []
             startPoint += degree
     
         #generate center hole
         cv.create_oval(center, fill="white")
         cv.create_arc(centerShadow, style="arc", start=71, extent=135)
-    
-        #generate label
-        # for x in label:
-             
 
         cv.pack()
         root.mainloop()
+
+    def pieData(data2):
+        rate = []
+        s = sum(data2)
+        for x in data2:
+            rate.append('{:.4f}'.format(x/s))
+        rate = numpy.array(rate, dtype = numpy.float64)
+        return rate
+    
+    def locationCal(degree):
+        rad = math.radians(degree)
+        xLocation1 = round(round(math.cos(-rad),2) * (chart3d.xx2-chart3d.xx1)/4 + (chart3d.xx2+chart3d.xx1)/2)
+        yLocation1 = round(round(math.sin(-rad),2) * (chart3d.xx2-chart3d.xx1)/4 + (chart3d.yy2+chart3d.yy1)/2)
+        xLocation2 = round(round(math.cos(-rad),2) * (chart3d.xx2-chart3d.xx1)/2-10 + (chart3d.xx2+chart3d.xx1)/2)
+        yLocation2 = round(round(math.sin(-rad),2) * (chart3d.xx2-chart3d.xx1)/2-10 + (chart3d.yy2+chart3d.yy1)/2)
+        return [xLocation1, yLocation1, xLocation2, yLocation2]
 
 def main():
      print('test main')
--- ww.py
+++ ww.py
@@ -3,9 +3,10 @@ from py3dchart import chart3d
 chart1 = chart3d()
 a = [5, 3, 1]
 b = ['q', 'w', 'e']
-chart1.x1(50)
-chart1.x2(300)
-chart1.y1(50)
-chart1.y2(300)
+# chart1.fontSize(12)
+# chart1.x1(50)
+# chart1.x2(300)
+# chart1.y1(50)
+# chart1.y2(300)
 chart1.drawPie(a, b)
 
