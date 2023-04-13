d815c4b15e6b9b11668a930fc5703066647a435d
Nick <nickyoung0017@outlook.com>
2023-04-13 22:23:36 +0800

    the first complete version

--- py3dchart/chart.py
+++ py3dchart/chart.py
@@ -1,10 +1,36 @@
 from tkinter import *
 import numpy
+import math
 
-__version__ = '0.1.8'
+__version__ = '0.2.1'
 __author__ = 'WangLvyuan'
 
-class threeDChart:
+class chart3d:
+    fontSize = 12
+    x1 = 20
+    y1 = 20
+    x2 = 220
+    y2 = 220
+
+    def fontSize(self, num):
+         chart3d.fontSize = num
+         return
+
+    def x1(self, num):
+         chart3d.x1 = num
+         return
+    
+    def x2(self, num):
+         chart3d.x2 = num
+         return
+    
+    def y1(self, num):
+         chart3d.y1 = num
+         return
+    
+    def y2(self, num):
+         chart3d.y2 = num
+         return
 
     def pieData(data2):
         rate = []
@@ -14,27 +40,48 @@ class threeDChart:
         rate = numpy.array(rate, dtype = numpy.float64)
         return rate
     
+    def locationCal(degree):
+        rad = math.radians(degree)
+        xLocation1 = round(round(math.cos(-rad),2) * (chart3d.x2-chart3d.x1)/4 + (chart3d.x2+chart3d.x1)/2)
+        yLocation1 = round(round(math.sin(-rad),2) * (chart3d.x2-chart3d.x1)/4 + (chart3d.y2+chart3d.y1)/2)
+        xLocation2 = round(round(math.cos(-rad),2) * (chart3d.x2-chart3d.x1)/2-10 + (chart3d.x2+chart3d.x1)/2)
+        yLocation2 = round(round(math.sin(-rad),2) * (chart3d.x2-chart3d.x1)/2-10 + (chart3d.y2+chart3d.y1)/2)
+        return [xLocation1, yLocation1, xLocation2, yLocation2]
+    
     # the pie apart
-    def pieGernerate(self, pie_data_as_array):
-        rate = threeDChart.pieData(pie_data_as_array)
+    def drawPie(self, pie_data_as_array, pie_lable_as_llist):
+        rate = chart3d.pieData(pie_data_as_array)
         root = Tk()
-        coord = 20, 20, 220, 220
-        shadow = 30, 30, 230, 230
-        center = 100, 100, 140, 140
-        centerShadow = 110, 110, 150, 150
+        coord = chart3d.x1, chart3d.y1, chart3d.x2, chart3d.y2
+        shadow = chart3d.x1 + 10, chart3d.y1 + 10, chart3d.x2 + 10, chart3d.y2 + 10
+        center = chart3d.x1 + 80, chart3d.y1 + 80, chart3d.x2 - 80, chart3d.y2 - 80
+        centerShadow = chart3d.x1 + 90, chart3d.y1 + 90, chart3d.x2 - 70, chart3d.y2 - 70
         cv = Canvas(root, bg='white')
+        cv.pack(fill = 'both',expand ='yes')
     
         # the 3D shadow, will not move
         cv.create_oval(shadow, fill="white")
+
+        #generate pie
         startPoint: float = 0
-        for x in rate:
+        for x,y in zip(rate, pie_lable_as_llist):
             degree: float = float('{:.2f}'.format(x*360))
+            print(chart3d.locationCal(startPoint + degree/2)) 
+            location = chart3d.locationCal(startPoint + degree/2)#data type: int in list
             cv.create_arc(coord, start=startPoint, extent=degree, fill="white")
+            cv.create_text(location[0], location[1], text=str(round((x*100),1)) + '%')
+            cv.create_text(location[2], location[3], text=y, font=("Arial", chart3d.fontSize))
+            location = []
             startPoint += degree
     
+        #generate center hole
         cv.create_oval(center, fill="white")
         cv.create_arc(centerShadow, style="arc", start=71, extent=135)
     
+        #generate label
+        # for x in label:
+             
+
         cv.pack()
         root.mainloop()
 
--- ww.py
+++ ww.py
@@ -1,5 +1,11 @@
-from py3dchart import threeDChart
+from py3dchart import chart3d
 
-chart = threeDChart()
-a = [1, 3, 5]
-chart.pieGernerate(a)
+chart1 = chart3d()
+a = [5, 3, 1]
+b = ['q', 'w', 'e']
+chart1.x1(50)
+chart1.x2(300)
+chart1.y1(50)
+chart1.y2(300)
+chart1.drawPie(a, b)
+
